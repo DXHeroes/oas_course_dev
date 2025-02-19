3e088e4d812961e6db7979e7e8ed9a5fce9c1e0c
Martin Albert <martin.albert.187@gmail.com>
2025-02-19 07:46:18 +0100

    java example base polishing

--- java_example/src/main/java/dev/dxheroes/dto/Promotion.java
+++ java_example/src/main/java/dev/dxheroes/dto/Promotion.java
@@ -1,20 +1,25 @@
 package dev.dxheroes.dto;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
-    @JsonSubTypes.Type(value = DiscountPromotion.class, name = "DISCOUNT"),
-    @JsonSubTypes.Type(value = FreeItemPromotion.class, name = "FREE_ITEM")
+    @JsonSubTypes.Type(value = DiscountPromotion.class, name = "discount"),
+    @JsonSubTypes.Type(value = BuyOneGetOnePromotion.class, name = "bogo")
 })
-public sealed interface Promotion permits DiscountPromotion, FreeItemPromotion {}
+public sealed interface Promotion permits DiscountPromotion, BuyOneGetOnePromotion {}
 
 record DiscountPromotion(
-    @NotNull Double amount
+    @NotNull
+    @JsonProperty(value = "amount", required = true)
+    Double amount
 ) implements Promotion {}
 
-record FreeItemPromotion(
-    @NotNull String description
+record BuyOneGetOnePromotion(
+    @NotNull
+    @JsonProperty(value = "description", required = true)
+    String description
 ) implements Promotion {}
